b55be4c70ce5c1850bc189132ccba6754aaa8694
SugiuraMakoto <margin9876@gmail.com>
2019-05-23 14:08:00 +0900

    クリアシーンを追加

---
+++ CESA2019/Assets/Scrips/playscene/SizeChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SizeChange : MonoBehaviour
+{
+    [SerializeField, Header("サイズ範囲")]
+    private float _minSize;
+
+    [SerializeField]
+    private float _maxSize;
+
+    private float _value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _value = 0.001f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += new Vector3(_value, _value);
+
+        if(transform.localScale.x < _minSize || transform.localScale.x > _maxSize)
+        {
+            _value *= -1;
+        }
+    }
+}
