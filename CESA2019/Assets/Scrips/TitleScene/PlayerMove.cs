0c9b9e04adc45e018428ca7cadcbca9625fffbb9
SyunsukeGoto <gtoshun@gmail.com>
2019-05-24 04:21:27 +0900

    TitleSceneの完成 セレクトシーンへのシーン移動はAkeyで移動するように仮設定してあります。 シーン名は外から入力できるようになっています

--- CESA2019/Assets/Scrips/TitleScene/PlayerMove.cs
+++ CESA2019/Assets/Scrips/TitleScene/PlayerMove.cs
@@ -8,6 +8,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 ///-----------------------------------------------------------------------------
 /// 
@@ -18,21 +19,25 @@ namespace Goto
 {
     public class PlayerMove : MonoBehaviour
     {
-        private float _vec;
-        private Vector3 _myPos;
-        private float _scaleX;
+        private float _vec;                 // 移動量
+        private Vector3 _myPos;             // 自分の座標
         [SerializeField]
-        private float _maxPos;
+        private float _maxPos;              // 右端の座標
         [SerializeField]
-        private float _minPos;
+        private float _minPos;              // 左端の座標
+        private Animator _animator;         // プレイヤーのアニメーター
+        [SerializeField]
+        private GameObject _starObject;
+        private string _imageName;
 
         /// <summary>
         /// 初期化処理
         /// </summary>
         void Start()
         {
-            _vec = 3;
-            _scaleX = 1;
+            _vec = 3;       // 移動量の設定
+            _animator = GetComponent<Animator>();
+            _imageName = "ChasedPlayer_1";
         }
 
         /// <summary>
@@ -40,17 +45,40 @@ namespace Goto
         /// </summary>
         void Update()
         {
+            // 自分座標の更新
             _myPos = gameObject.GetComponent<RectTransform>().anchoredPosition;
 
+            // 移動範囲外を超えたら移動量を逆にする
             if (_myPos.x >= _maxPos || _myPos.x <= _minPos)
             {
                 _vec *= -1;
-                _scaleX *= -1;
             }
 
+            if(_vec < 0)
+            {
+                _animator.SetBool("Chased", true);
+                _starObject.SetActive(false);
+            }
+            if (_vec > 0)
+            {
+                _animator.SetBool("Chased", false);
+            }
+
+            //if(gameObject.ima)
+            Image img = GameObject.Find("Canvas/Player").GetComponent<Image>();
+
+            if (img.sprite.name == _imageName)
+            {
+                _starObject.SetActive(true);
+            }
+            else
+            {
+                _starObject.SetActive(false);
+            }
+
+            // プレイヤーの移動等の更新の反映
             float posX = _myPos.x + _vec;
             gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, _myPos.y, _myPos.z);
-            gameObject.GetComponent<RectTransform>().localScale = new Vector3(_scaleX, 1f, 1f);
         }
     }
 }
---
+++ CESA2019/Assets/Scrips/TitleScene/TransitionToSelectScene.cs
@@ -0,0 +1,45 @@
+﻿//__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__
+/// <file>		TransitionToSelectScene.cs
+/// 
+/// <date>		2019/5/22
+/// 
+/// <author>	後藤　駿介
+//__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+///-----------------------------------------------------------------------------
+/// 
+/// </brief> シーン遷移クラス
+///
+///-----------------------------------------------------------------------------
+namespace Goto
+{
+    public class TransitionToSelectScene : MonoBehaviour
+    {
+        [SerializeField]
+        private string _selectSceneName;
+
+
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        void Start()
+        {
+
+        }
+
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                SceneManager.LoadScene(_selectSceneName);
+            }
+        }
+    }
+}
