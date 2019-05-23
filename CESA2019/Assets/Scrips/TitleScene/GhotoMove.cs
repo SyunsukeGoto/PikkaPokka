49da799a2848fd3bf63ec0b5ad4af5f8756990aa
SyunsukeGoto <gtoshun@gmail.com>
2019-05-23 13:26:04 +0900

    更新

--- CESA2019/Assets/Scrips/TitleScene/GhotoMove.cs
+++ CESA2019/Assets/Scrips/TitleScene/GhotoMove.cs
@@ -14,41 +14,48 @@ using UnityEngine;
 /// </brief> おばけクラス
 ///
 ///-----------------------------------------------------------------------------
-public class GhotoMove : MonoBehaviour
+namespace Goto
 {
-    private float _angle;
-    private float _startPos;
-    private float _vec;
-    private Vector3 _myPos;
-    private float _scaleX;
-    /// <summary>
-    /// 初期化処理
-    /// </summary>
-    void Start()
+    public class GhotoMove : MonoBehaviour
     {
-        _angle = 0f;
-        _startPos = gameObject.GetComponent<RectTransform>().anchoredPosition.y;
-        _vec = 3;
-        _scaleX = 1;
-    }
-
-    /// <summary>
-    /// 更新処理
-    /// </summary>
-    void Update()
-    {
-        _myPos = gameObject.GetComponent<RectTransform>().anchoredPosition;
-
-        if(_myPos.x >= 614 || _myPos.x <= -903)
+        private float _angle;
+        private float _startPos;
+        private float _vec;
+        private Vector3 _myPos;
+        private float _scaleX;
+        [SerializeField]
+        private float _maxPos;
+        [SerializeField]
+        private float _minPos;
+        
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        void Start()
         {
-            _vec *= -1;
-            _scaleX *= -1;
+            _angle = 0f;
+            _startPos = gameObject.GetComponent<RectTransform>().anchoredPosition.y;
+            _vec = 3;
+            _scaleX = 1;
         }
 
-        float posX = _myPos.x + _vec;
-        _angle += 0.05f;
-        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, (Mathf.Sin(_angle) * 20) + _startPos, _myPos.z);
-        gameObject.GetComponent<RectTransform>().localScale = new Vector3(_scaleX, 1f, 1f);
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+            _myPos = gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            if (_myPos.x >= _maxPos || _myPos.x <= _minPos)
+            {
+                _vec *= -1;
+                _scaleX *= -1;
+            }
+
+            float posX = _myPos.x + _vec;
+            _angle += 0.05f;
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, (Mathf.Sin(_angle) * 20) + _startPos, _myPos.z);
+            gameObject.GetComponent<RectTransform>().localScale = new Vector3(_scaleX, 1f, 1f);
+        }
     }
 }
-
--- CESA2019/Assets/Scrips/TitleScene/PlayerMove.cs
+++ CESA2019/Assets/Scrips/TitleScene/PlayerMove.cs
@@ -14,35 +14,43 @@ using UnityEngine;
 /// </brief> プレイヤークラス
 ///
 ///-----------------------------------------------------------------------------
-public class PlayerMove : MonoBehaviour
+namespace Goto
 {
-    private float _vec;
-    private Vector3 _myPos;
-    private float _scaleX;
-    /// <summary>
-    /// 初期化処理
-    /// </summary>
-    void Start()
+    public class PlayerMove : MonoBehaviour
     {
-        _vec = 3;
-        _scaleX = 1;
-    }
+        private float _vec;
+        private Vector3 _myPos;
+        private float _scaleX;
+        [SerializeField]
+        private float _maxPos;
+        [SerializeField]
+        private float _minPos;
 
-    /// <summary>
-    /// 更新処理
-    /// </summary>
-    void Update()
-    {
-        _myPos = gameObject.GetComponent<RectTransform>().anchoredPosition;
-
-        if (_myPos.x >= 974f || _myPos.x <= -543f)
+        /// <summary>
+        /// 初期化処理
+        /// </summary>
+        void Start()
         {
-            _vec *= -1;
-            _scaleX *= -1;
+            _vec = 3;
+            _scaleX = 1;
         }
 
-        float posX = _myPos.x + _vec;
-        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, _myPos.y, _myPos.z);
-        gameObject.GetComponent<RectTransform>().localScale = new Vector3(_scaleX, 1f, 1f);
+        /// <summary>
+        /// 更新処理
+        /// </summary>
+        void Update()
+        {
+            _myPos = gameObject.GetComponent<RectTransform>().anchoredPosition;
+
+            if (_myPos.x >= _maxPos || _myPos.x <= _minPos)
+            {
+                _vec *= -1;
+                _scaleX *= -1;
+            }
+
+            float posX = _myPos.x + _vec;
+            gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(posX, _myPos.y, _myPos.z);
+            gameObject.GetComponent<RectTransform>().localScale = new Vector3(_scaleX, 1f, 1f);
+        }
     }
 }
