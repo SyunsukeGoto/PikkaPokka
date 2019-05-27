8fd2753bee803a214e0d3d75159929838ca3ea33
TamamuraSyuuki <syuuki0105@gmail.com>
2019-05-27 16:23:02 +0900

    コーンの破壊エフェクトを追加

--- CESA2019/Assets/Scrips/playscene/CrushableBox.cs
+++ CESA2019/Assets/Scrips/playscene/CrushableBox.cs
@@ -11,11 +11,19 @@ namespace Momoya
         private float _createZone = 1.0f;
         private float _addPower = 200.0f;
         bool _dethflag;
+
+        // Actor: Tamamura Shuuki
+        // Add: カラーコーンコンポーネントのへの参照  
+        // カラーコーンが壊れる時に使います
+        private ColorCone _colorCone;
+
         // Start is called before the first frame update
         void Start()
         {
             _createNum = 0;
             _dethflag = false;
+
+            _colorCone = GetComponent<ColorCone>();
         }
 
         // Update is called once per frame
@@ -29,7 +37,16 @@ namespace Momoya
                     go.transform.position = this.transform.position + new Vector3(0, 1.0f, 0);
                     go.transform.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-_createZone, _createZone), Random.Range(-_createZone, _createZone), Random.Range(-_createZone, _createZone)) * _addPower);
                 }
-                Destroy(this.gameObject);
+
+                // Actor: Tamamura Shuuki
+                // Add: コーンの破壊処理を追加
+                // カラーコーンを壊すエフェクトを発動し、コライダとリジッドボディを削除します。
+                // オブジェクトは破棄しません
+                _colorCone.Destruction();
+                Destroy(GetComponent<Rigidbody>());
+                Destroy(GetComponent<BoxCollider>());
+
+                _dethflag = false;
             }
         }
         //死ぬための関数
