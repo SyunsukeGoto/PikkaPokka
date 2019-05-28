e60c541f8d3955ca44dad9fb01653482b04bfd13
TamamuraSyuuki <syuuki0105@gmail.com>
2019-05-28 11:03:30 +0900

    デバッグ（Qキー）でドキドキが実装されたよ

---
+++ CESA2019/Assets/Scrips/Camera/PlayCamera.cs
@@ -0,0 +1,38 @@
+﻿//
+// PlayCamera.cs
+// Actor: Tamamura Shuuki
+//
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// プレイシーンに使用するカメラ
+public class PlayCamera : MonoBehaviour
+{
+
+    private Terribly _teriibly;
+
+    
+    void Start()
+    {
+        _teriibly = GetComponent<Terribly>();
+    }
+
+    // ----------------------------------------------
+    // ドキドキカメラを再生する
+    // ----------------------------------------------
+    public void OnTerribly()
+    {
+        _teriibly.Play();
+    }
+
+    // ----------------------------------------------
+    // カメラに適用しているエフェクトを戻す
+    // ----------------------------------------------
+    public void Exit()
+    {
+        _teriibly.Stop();
+    }
+}
