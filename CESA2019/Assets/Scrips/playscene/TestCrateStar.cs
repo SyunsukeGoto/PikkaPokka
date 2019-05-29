e0aa6b0a4128e7e3a57c254d750ca0809932cfa7
SugiuraMakoto <margin9876@gmail.com>
2019-05-30 03:48:24 +0900

    なおしたよーん

--- CESA2019/Assets/Scrips/playscene/TestCrateStar.cs
+++ CESA2019/Assets/Scrips/playscene/TestCrateStar.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class TestCrateStar : MonoBehaviour
 {
     public GameObject star;
+    public bool _flag;
+    [SerializeField ,Range(0.1f,1f)]
+    private float _interval = 0.5f;
     int num = 5;
+    float _time;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,13 +19,19 @@ public class TestCrateStar : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(_flag)  
         {
-            for (int i = 0; i < num; i++)
+            Debug.Log("1agrfd");
+            _time += Time.deltaTime;
+            if (_time >= _interval)
             {
-                GameObject go = Instantiate(star) as GameObject;
-                go.transform.position = this.transform.position + new Vector3(0, 1.0f, 0);
-                go.transform.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f),Random.Range(-1.0f,1.0f)) * 200);
+                _time = 0f;
+                for (int i = 0; i < num; i++)
+                {
+                    GameObject go = Instantiate(star) as GameObject;
+                    go.transform.position = this.transform.position + new Vector3(0, 1.0f, 0);
+                    go.transform.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)) * 200);
+                }
             }
 
         }
