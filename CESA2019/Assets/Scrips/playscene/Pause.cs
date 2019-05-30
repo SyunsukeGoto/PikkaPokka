caae23b0633c8d5d84562d62b900fe7da716a11a
SugiuraMakoto <margin9876@gmail.com>
2019-05-30 15:19:53 +0900

    ポーズ追加

---
+++ CESA2019/Assets/Scrips/playscene/Pause.cs
@@ -0,0 +1,194 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3[] _exitPos = new Vector3[2];
+
+    [SerializeField]
+    private Vector3[] _selectPos = new Vector3[3];
+
+    [SerializeField]
+    private Image _image;
+
+    [SerializeField]
+    private Sprite[] _sprites = new Sprite[2];
+
+    [SerializeField]
+    private Fade _fade;
+
+    [SerializeField]
+    private Image _hammer;
+
+    public enum Mode
+    {
+        Exit,
+        Select,
+    }
+
+    [SerializeField]
+    private Mode _mode;
+
+    private enum Choice
+    {
+        Decision,
+        ReStart,
+        Return,
+    }
+
+    private int _choice;
+
+    public enum State
+    {
+        NONE,
+        DISPLAY,
+    }
+
+    [SerializeField]
+    private State _state;
+
+    public State STATE
+    {
+        set
+        {
+            _state = value;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _image.sprite = _sprites[(int)_mode];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_state == State.DISPLAY)
+        {
+            Color color = _image.color;
+            color.a = 1;
+            _image.color = color;
+            _hammer.color = color;
+
+            Time.timeScale = 0;
+
+            switch (_mode)
+            {
+                case Mode.Exit:
+                    Exit();
+                    break;
+                case Mode.Select:
+                    Select();
+                    break;
+            }
+        }
+        else
+        {
+            Color color = _image.color;
+            color.a = 0;
+            _image.color = color;
+            _hammer.color = color;
+
+            Time.timeScale = 1;
+
+            if(Input.GetButtonDown("Start"))
+            {
+                _state = State.DISPLAY;
+            }
+        }
+    }
+
+    private void Exit()
+    {
+        if (Input.GetAxis("Vertical") <= -1)
+        {
+            if (_choice != (int)Choice.Decision)
+            {
+                _choice -= 2;
+            }
+        }
+        else if (Input.GetAxis("Vertical") >= 1)
+        {
+            if (_choice != (int)Choice.Return)
+            {
+                _choice += 2;
+            }
+        }
+        else if (Input.GetButtonDown("Z"))
+        {
+            switch ((Choice)_choice)
+            {
+                case Choice.Decision:
+                    Application.Quit();
+                    break;
+                case Choice.Return:
+                    _state = State.NONE;
+                    break;
+            }
+        }
+
+        switch ((Choice)_choice)
+        {
+            case Choice.Decision:
+                _hammer.GetComponent<RectTransform>().localPosition = _exitPos[0];
+                break;
+            case Choice.Return:
+                _hammer.GetComponent<RectTransform>().localPosition = _exitPos[1];
+                break;
+        }
+    }
+
+    private void Select()
+    {
+        if (Input.GetAxis("Vertical") <= -1)
+        {
+            if (_choice != (int)Choice.Decision)
+            {
+                _choice--;
+            }
+        }
+        else if (Input.GetAxis("Vertical") >= 1)
+        {
+            if (_choice != (int)Choice.Return)
+            {
+                _choice++;
+            }
+        }
+        else if (Input.GetButtonDown("Z"))
+        {
+            switch ((Choice)_choice)
+            {
+                case Choice.Decision:
+                    SceneManager.LoadScene("Select");
+                    break;
+                case Choice.ReStart:
+                    string name = SceneManager.GetActiveScene().name;
+                    SceneManager.LoadScene(name);
+                    break;
+                case Choice.Return:
+                    _state = State.NONE;
+                    break;
+            }
+        }
+
+        switch((Choice)_choice)
+        {
+            case Choice.Decision:
+                _hammer.GetComponent<RectTransform>().localPosition = _selectPos[0];
+                break;
+            case Choice.ReStart:
+                _hammer.GetComponent<RectTransform>().localPosition = _selectPos[1];
+                break;
+            case Choice.Return:
+                _hammer.GetComponent<RectTransform>().localPosition = _selectPos[2];
+                break;
+        }
+    }
+
+
+}
